7638052872b489152322716b662f9f3b0e3c502c
herme <herme@eduroam104108.eduroam.ucm.es>
2018-11-21 10:51:16 +0100

    update resueltos de AC

--- practicasDeClase/moleculas.cpp
+++ practicasDeClase/moleculas.cpp
@@ -1,7 +1,7 @@
+#include <iostream>
+using namespace std;
 
-
-
-int sizeOfCicle(int ini, int v[],bool black[])
+long long int sizeOfCicle(int ini, int v[],bool black[])
 {
     int r = 1;
     black[ini] = true;
@@ -15,19 +15,38 @@ int sizeOfCicle(int ini, int v[],bool black[])
     return r;
 }
 
-int numOfSteps(int v[],int n)
+long long int numOfSteps(int v[],int n)
 {
     bool black[INT_MAX];
     for(int i = 0; i < n; i++)black[i] = false;
-    int ret = 1;
+    long long int ret = 1;
 
     int j = 0;
     while(j < n)
     {
-        int curr = sizeOfCicle(j,v,black);
+        long long int curr = sizeOfCicle(j,v,black);
         if(ret%curr != 0 && curr%ret != 0) ret *= curr;
         else if (ret < curr) ret = curr;
         while(black[j] && j < n) j++;
     }
     return ret;
+}
+
+int main()
+{
+    int v[100];
+    long long int n;
+    cin << n;
+    while( n != 0)
+    {
+        int i = 0;
+        while(i < n)
+        {
+            cin << v[i];
+            i++;
+        }
+        cout << numOfSteps(v,n) << "\n";
+        cin << n;
+    }
+    return 0;
 }
